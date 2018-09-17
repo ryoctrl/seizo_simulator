a0b7d37bfd12545348bad2be9bdc19c086fc6690
mosin <ryoctrl@gmail.com>
2018-09-17 18:02:07 +0900

    一旦あげ

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/seizo', (req, res, next) => {
+	let jinriki = req.params.jinriki;
+	let danyaku = req.params.danyaku;
+	let haikyu = req.params.haikyu;
+	let parts = req.params.parts;
+
+	if (!jinriki || !danyaku || !haikyu || !parts) {
+		res.json({
+			"error": `製造用パラメータが足りない.`
+		});
+		return;
+	}
+
+
+	
+
+});
+
 module.exports = router;
